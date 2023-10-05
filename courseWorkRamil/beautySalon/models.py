f3e077d2d622b26d1eea474a0eb2877857d7886c
PereverzevIvan <peregh320@gmail.com>
2023-10-05 16:01:14 +0300

    Модернизация БД, настройка админки

--- courseWorkRamil/beautySalon/models.py
+++ courseWorkRamil/beautySalon/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 
 # Классы моделей
-
 class Specialization(models.Model):
     ''' Модель для описания в базе данных таблицы со специализациями,
      к которым будут относиться процедуры и мастера '''
     name = models.CharField(max_length=200, verbose_name="Название")
 
     def __str__(self):
-        return f'Специализация {self.name}'
+        return f'{self.name}'
 
     class Meta:
         verbose_name = "Специализация"
@@ -19,11 +21,11 @@ class Specialization(models.Model):
 class Master(models.Model):
     ''' Модель для описания в базе данных таблицы с информацией о мастерах '''
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
-    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE, verbose_name='Специализация')
-    image = models.ImageField(upload_to=f'images/masters/{user}', verbose_name='Изображение')
+    image = models.ImageField(upload_to=f'images/masters/', verbose_name='Изображение')
+    specialization = models.ManyToManyField(Specialization, verbose_name='Специализации')
 
     def __str__(self):
-        return f'Мастер-{self.specialization.name}, {self.user.name}'
+        return f'Мастер-{self.specialization.name}, {self.user.username}'
 
     class Meta:
         verbose_name = "Мастер"
@@ -36,10 +38,11 @@ class Service(models.Model):
     name = models.CharField(max_length=200, verbose_name="Название")
     description = models.TextField(verbose_name='Описание')
     price = models.FloatField(verbose_name='Стоимость')
-    image = models.ImageField(upload_to=f'images/services/{name}', verbose_name='Изображение')
+    duration = models.IntegerField(null=False, blank=False, verbose_name='Длительность, мин')
+    specialization = models.ForeignKey(Specialization, null=False, blank=False, on_delete=models.PROTECT, verbose_name='Сепциализация')
 
     def __str__(self):
-        return f'Процедура "{self.name}"'
+        return f'"{self.name}"'
 
     class Meta:
         verbose_name = "Процедура"
@@ -100,3 +103,85 @@ class Article(models.Model):
     class Meta:
         verbose_name = "Статью"
         verbose_name_plural = "Статьи"
+
+
+# Классы админки
+class ServiceInlines(admin.TabularInline):
+    model = Service
+    fields = ['name', 'duration', 'price']
+
+class CommentInlines(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'text']
+
+
+class SpecializationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ['name']
+    inlines = [ServiceInlines]
+
+
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'get_image')
+    list_display_links = ('id', 'user')
+    search_fields = ['user__username', 'specialization']
+    filter_horizontal = ['specialization']
+    list_filter = ['specialization']
+
+    inlines = [CommentInlines]
+
+    def get_image(self, object):
+        return mark_safe(f'<img src="/static/{object.image}" width=120>')
+    
+    get_image.short_description = 'Изображение'
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'duration', 'price', 'specialization')
+    list_display_links = ('id', 'name')
+    list_filter = ['specialization']
+    search_fields = ['name', 'specialization']
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'time', 'user', 'master', 'service')
+    list_display_links = ('id', 'date', 'time')
+    list_filter = ['service']
+    search_fields = ['user__username', 'master__username']
+    date_hierarchy = "date"
+
+
+class MasterRatingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_info', 'value')
+    list_display_links = ('id', 'get_info')
+    search_fields = ['evaluating__username', 'evaluated__username']
+    list_filter = ['value']
+
+    def get_info(self, object):
+        return f'Пользователь {object.evaluating.username} оценил пользователя {object.evaluated.username}'
+    
+    get_info.short_description = 'Информация'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'master', 'created_at')
+    list_display_links = ('id', 'author')
+    search_fields = ['author__username', 'master__username']
+    readonly_fields = ['author', 'master', 'text', 'created_at']
+    date_hierarchy = "created_at"
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'get_image')
+    list_display_links = ('id', 'title')
+    search_fields = ['title']
+    readonly_fields = ['created_at']
+    list_filter = ['created_at']
+    date_hierarchy = "created_at"
+
+    def get_image(self, object):
+        return mark_safe(f'<img src="/static/{object.image}" width=70>')
+    
+    get_image.short_description = 'Изображение'
+
--- courseWorkRamil/courseWorkRamil/settings.py
+++ courseWorkRamil/courseWorkRamil/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = 'mainApp/static/'
+MEDIA_URL = 'beautySalon/static/'
 MEDIA_ROOT = BASE_DIR / MEDIA_URL
